8d96135676c2a22c43f32b23adf15e654002e0ba
juliojavalos <javalos@fctunca.edu.py>
2024-03-12 15:58:28 -0300

    Commit

--- sim/models.py
+++ sim/models.py
@@ -1,3 +1,74 @@
 from django.db import models
 
-# Create your models here.
+
+# class Usuario(models.Model):
+# idUsuario = models.IntegerField(blank=True, null=True)
+# Nombre = models.CharField(max_length=15, blank=True, null=True)
+# correo = models.EmailField(max_length=20, blank=True, null=True)
+# password = models.CharField(max_length=20, blank=True, null=True)
+
+# def __str__(self):
+# return self.name  # Corregido el método str
+
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    correo_electronico = models.EmailField(max_length=100)
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
+
+class Facultad(models.Model):
+    id = models.AutoField(primary_key=True)
+    Descripcion = models.CharField(max_length=50)
+
+
+class Carrera(models.Model):
+    id = models.AutoField(primary_key=True)
+    Descripcion = models.CharField(max_length=50)
+    idFacultad = models.ForeignKey(Facultad, on_delete=models.CASCADE)
+
+
+class Materia(models.Model):
+    id = models.AutoField(primary_key=True)
+    Descripcion = models.CharField(max_length=50)
+    idCarrera = models.ForeignKey(Carrera, on_delete=models.CASCADE)
+    Semestre = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.Descripcion}{self.Semestre}"
+
+
+class Docente(models.Model):
+    id = models.AutoField(primary_key=True)
+    Nombre = models.CharField(max_length=50)
+    Apellido = models.CharField(max_length=50)
+
+
+class Edificio(models.Model):
+    id = models.AutoField(primary_key=True)
+    Descripcion = models.CharField(max_length=50)
+
+
+class Planta(models.Model):
+    id = models.AutoField(primary_key=True)
+    Descripcion = models.CharField(max_length=50)
+    idEdificio = models.ForeignKey(Edificio, on_delete=models.CASCADE)
+
+
+class Sala(models.Model):
+    id = models.AutoField(primary_key=True)
+    Descripcion = models.CharField(max_length=50)
+    idPlanta = models.ForeignKey(Planta, on_delete=models.CASCADE)
+
+
+class Actividades(models.Model):
+    id = models.AutoField(primary_key=True)
+    Descripcion = models.CharField(max_length=50)
+    idSala = models.ForeignKey(Sala, on_delete=models.CASCADE)
+    idUsuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    fecha_actividad = models.DateTimeField()
+    fecha_notificacion = models.DateTimeField()
--- sim/views.py
+++ sim/views.py
@@ -1,3 +1,66 @@
+import os
+from .models import User
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+from google.oauth2 import id_token
+from google.auth.transport import requests
 from django.shortcuts import render
+from .models import Materia
+import jwt
 
-# Create your views here.
+GOOGLE_OAUTH_CLIENT_ID = "425881363668-ch0d9plss8pnoukc95a22rpdj54bgaot.apps.googleusercontent.com"
+
+
+def sign_in(request):
+    return render(request, 'sign_in.html')
+
+
+def inicio(request):
+    return render(request, 'inicio.html')
+
+
+@csrf_exempt
+def auth_receiver(request):
+    """
+    Google calls this URL after the user has signed in with their Google account.
+    """
+    token = request.POST.get('credential')
+
+    try:
+        user_data = id_token.verify_oauth2_token(
+            token, requests.Request(), GOOGLE_OAUTH_CLIENT_ID
+        )
+    except ValueError:
+        # Si el token no es válido, retorna una respuesta HTTP con el estado 403 (Forbidden)
+        return HttpResponse(status=403)
+    except Exception as e:
+        # Captura cualquier otra excepción y maneja el error de manera adecuada
+        # Puedes imprimir el error para depuración o registrar el error en tu sistema de registro
+        print(f"Ocurrió un error durante la verificación del token: {e}")
+        return HttpResponse(status=500)
+
+    # Si la verificación del token fue exitosa, almacena los datos del usuario en la sesión y redirige
+    request.session['user_data'] = user_data
+    return redirect('inicio')
+    correo_electronico = user_data.get('email')
+    if User.objects.filter(correo_electronico=correo_electronico).exists():
+        print("cuenta ya existente")
+        pass
+    else:
+        nombre = user_data.get('given_name')
+        apellido = user_data.get('family_name')
+        nuevo_usuario = User(nombre=nombre, apellido=apellido, correo_electronico=correo_electronico)
+        nuevo_usuario.save()
+
+
+def sign_out(request):
+    del request.session['user_data']
+    print("Sesion Cerrada")
+    return redirect('sign_in')
+
+
+def mostrar_materias(request):
+    materias = Materia.objects.all()
+
+    return render(request, 'mostrar_materias.html', {'materias': materias})
