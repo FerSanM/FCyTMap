82a63114e774340197b7e3ef2d0e321b3755c88f
FernandoSan26 <ferchu.sanabria.2015@gmail.com>
2024-03-18 17:51:43 -0300

    se puso unas correcciones al menu

--- MapaFcyT/settings.py
+++ MapaFcyT/settings.py
@@ -71,7 +71,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "is2",
         "USER": "postgres",
-        "PASSWORD": "5675437",
+        "PASSWORD": "260818",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
