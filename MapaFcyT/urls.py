add1aaf6704f588af8f282d468dd3577cfd016eb
JulioAvalos437 <avalosjulios437@gmail.com>
2024-04-25 21:18:18 -0400

    se agregaron las urls de django allauth

--- MapaFcyT/urls.py
+++ MapaFcyT/urls.py
@@ -1,8 +1,9 @@
-
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sim.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
