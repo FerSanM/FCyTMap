274af69733883122dd71b9fad056183cda468b47
JulioAvalos437 <avalosjulios437@gmail.com>
2024-07-29 00:38:27 -0400

    se agrego un form para el admin donde puede enviar eventos generales para los usuarios

--- sim/admin.py
+++ sim/admin.py
@@ -1,9 +1,36 @@
+# admin.py
 from django.contrib import admin
+from .models import Actividades
+from .forms import ActividadesForm
+from django.contrib.auth.models import User
+from datetime import timedelta
 from .models import RelacionMateriaSala, RelacionUsuarioMateria, Sala, Actividades , Materia
+class ActividadesAdmin(admin.ModelAdmin):
+    form = ActividadesForm
 
-#Register your models here.
+    def save_model(self, request, obj, form, change):
+        if not change:  # Si se está creando una nueva instancia
+            usuarios = User.objects.filter(is_staff=False)  # Obtener todos los usuarios no staff
+            fecha_actividad = form.cleaned_data['fecha_actividad']
+            fecha_notificacion = fecha_actividad - timedelta(weeks=1)  # Calcular fecha_notificacion como una semana antes
+
+            for usuario in usuarios:
+                Actividades.objects.create(
+                    descripcion=obj.descripcion,
+                    idSala=obj.idSala,
+                    idUsuario=usuario,
+                    fecha_actividad=fecha_actividad,
+                    fecha_notificacion=fecha_notificacion,
+                    vistousuario=obj.vistousuario
+                )
+        else:
+            # Para la edición de una actividad existente, usa el valor de fecha_actividad proporcionado
+            obj.fecha_notificacion = obj.fecha_actividad - timedelta(weeks=1)
+            super().save_model(request, obj, form, change)
+
+admin.site.register(Actividades, ActividadesAdmin)
+
 admin.site.register(RelacionMateriaSala)
 admin.site.register(RelacionUsuarioMateria)
 admin.site.register(Sala)
-admin.site.register(Actividades)
 admin.site.register(Materia)
